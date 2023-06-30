cd71dab7796fa8b77c931e72cfa2348791210496
jun0925 <yeongjun0925@gmail.com>
2023-06-30 14:58:06 +0900

    prop를 이용하여 부모컴포넌트에서 자식컴포넌트로 데이터 전달하기

--- src/views/NestedComponent.vue
+++ src/views/NestedComponent.vue
@@ -1,11 +1,16 @@
 <template>
   <div>
-    <PageTitle />
+    <PageTitle :title="title" />
   </div>
 </template>
 <script>
   import PageTitle from '../components/PageTitle.vue' // 컴포넌트 import
   export default {
+    data() {
+      return {
+        title: 'NestedComponent Title'
+      }
+    },
     components: {
       PageTitle // 현재 컴포넌트에서 사용할 컴포넌트 등록
     }
